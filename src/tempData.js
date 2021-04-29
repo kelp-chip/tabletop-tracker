29c4318e0452e5a62f542e7bf8ed437993bd31c4
Lexy Kramer <lexy.kramer@gmail.com>
2021-04-28 23:38:23 -0400

    fixed filenames

--- src/tempData.js
+++ src/tempData.js
@@ -42,46 +42,32 @@ let tempData = [
       "https://cdn.shopify.com/s/files/1/0900/3540/products/3d-wingspan.png?v=1551978393",
   },
   {
-    id: "TAAifFP590",
-    name: "Root",
-    price: "104.98",
-    min_players: 2,
+    id: "RLlDWHh7hR",
+    name: "Gloomhaven",
+    price: "89.99",
+    min_players: 1,
     max_players: 4,
     min_playtime: 60,
-    max_playtime: 90,
-    min_age: 10,
+    max_playtime: 150,
+    min_age: 12,
     description:
-      "<p>Find adventure in this marvelous asymmetric game. Root provides limitless replay value as you and your friends explore the unique factions all wanting to rule a fantastic forest kingdom. Play as the Marquise de Cat and dominate the woods, extracting its riches and policing its inhabitants, as the Woodland Alliance, gathering supporters and coordinate revolts against the ruling regime, the Eyrie Dynasties, regaining control of the woods while keeping your squabbling court at bay, or as the Vagabond, seeking fame and fortune as you forge alliances and rivalries with the other players. Each faction has its own play style and paths to victory, providing an immersive game experience you will want to play again and again.</p>",
+      "<p>Gloomhaven is a game of Euro-inspired tactical combat in a persistent world of shifting motives. Players will take on the role of a wandering adventurer with their own special set of skills and their own reasons for traveling to this dark corner of the world.<br /><br />Players must work together out of necessity to clear out menacing dungeons and forgotten ruins. In the process they will enhance their abilities with experience and loot, discover new locations to explore and plunder, and expand an ever-branching story fueled by the decisions they make.<br /><br />This is a legacy game with a persistent and changing world that is ideally played over many game sessions. After a scenario, players will make decisions on what to do, which will determine how the story continues, kind of like a &quot;Choose Your Own Adventure&quot; book. Playing through a scenario is a cooperative affair where players will fight against automated monsters using an innovative card system to determine the order of play and what a player does on their turn.</p>",
     image_url:
-      "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1540147295104",
+      "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254920151-51ulRXlJ7LL.jpg",
   },
   {
-    id: "yqR4PtpO8X",
-    name: "Scythe",
-    price: "56.30",
+    id: "fDn9rQjH9O",
+    name: "Terraforming Mars",
+    price: "51.99",
     min_players: 1,
     max_players: 5,
     min_playtime: 90,
     max_playtime: 120,
-    min_age: 14,
+    min_age: 12,
     description:
-      "<p><em>Scythe</em> gives players almost complete control over their fate. Other than each player's individual hidden objective card, the only elements of luck or variability are &quot;Encounter&quot; cards that players will draw as they interact with the citizens of newly explored lands. Each encounter card provides the player with several options, allowing them to mitigate the luck of the draw through their selection. Combat is also driven by choices, not luck or randomness.<br /><br /><em>Scythe</em> uses a streamlined action-selection mechanism (no rounds or phases) to keep gameplay moving at a brisk pace and reduce downtime between turns. While there is plenty of direct conflict for players who seek it, there is no player elimination.<br /><br />Every part of <em>Scythe</em> has an aspect of engine-building to it. Players can upgrade actions to become more efficient, build structures that improve their position on the map, enlist new recruits to enhance character abilities, activate mechs to deter opponents from invading, and expand their borders to reap greater types and quantities of resources. These engine-building aspects create a sense of momentum and progress throughout the game. The order in which players improve their engine adds to the unique feel of each game, even when playing one faction multiple times.</p>",
+      "<p>In the 2400s, mankind begins to terraform the planet Mars. Giant corporations, sponsored by the World Government on Earth, initiate huge projects to raise the temperature, the oxygen level and the ocean coverage until the environment is habitable. In Terraforming Mars you play one of those corporations and work together in the terraforming process, but compete in doing the best work, with victory points awarded not only for your contribution to the terraforming, but also for advancing human infrastructure throughout the solar systems and other commendable achievements. The players acquire unique project cards, which represent anything from introducing plant life or animals, hurling asteroids at the surface, building cities, and establishing greenhouse gas industries to heat up the atmosphere. You compete for the best places for your city tiles, ocean tiles and greenery tiles. When the terraforming process is complete, the player corporation with the most victory points wins.</p>",
     image_url:
-      "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254971843-513-lrmjs2L.jpg",
-  },
-  {
-    id: "5H5JS0KLzK",
-    name: "Wingspan",
-    price: "47.98",
-    min_players: 1,
-    max_players: 5,
-    min_playtime: 40,
-    max_playtime: 70,
-    min_age: 10,
-    description:
-      "<p><em><strong>Wingspan</strong></em> is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games.</p>\r\n<p>You are bird enthusiasts—researchers, bird watchers, ornithologists, and collectors—seeking to discover and attract the best birds to your aviary. Each bird extends a chain of powerful combinations in one of your habitats (actions). These habitats focus on several key aspects of growth:</p>\r\n<ul>\r\n<li>Gain food tokens via custom dice in a birdfeeder dice tower</li>\r\n<li>Lay eggs using egg miniatures in a variety of colors</li>\r\n<li>Draw from hundreds of unique bird cards and play them</li>\r\n</ul>\r\n<p>The winner is the player with the most points after 4 rounds.</p>",
-    image_url:
-      "https://cdn.shopify.com/s/files/1/0900/3540/products/3d-wingspan.png?v=1551978393",
+      "https://s3-us-west-1.amazonaws.com/5cc.images/games/uploaded/1559254934477-61ys4u7ejfL.jpg",
   },
 ];
 
