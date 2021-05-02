0522b3641cc6ca7ed68ca98389d5fdb5cf660aa8
Lexy Kramer <lexy.kramer@gmail.com>
2021-05-02 13:41:15 -0400

    init deploy

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,8 @@ function App() {
   const [games, setGames] = useState([]);
   const [game, setGame] = useState(null);
 
+  // const [minAge, setMinAge] = useState("")
+
   const handleSearchSubmit = (event) => {
     event.preventDefault();
     axios
