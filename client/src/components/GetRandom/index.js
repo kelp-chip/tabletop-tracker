2a27c3f2b33fd7002a1ab1c3695a9a70495e1b99
Lexy Kramer <lexy.kramer@gmail.com>
2021-07-16 12:30:22 -0400

    changed dice icon

--- client/src/components/GetRandom/index.js
+++ client/src/components/GetRandom/index.js
@@ -1,13 +1,21 @@
 import "./GetRandom.scss";
+import dice from "../../images/dice.svg";
 
 export default function GetRandom({ handleGetRandom }) {
   return (
     <div className="dice-container">
-      <i
+      <img
+        src={dice}
+        alt="dice"
+        onClick={() => handleGetRandom()}
+        width="50px"
+        className="dice"
+      ></img>
+      {/* <i
         className="fas fa-dice"
         title="get random game"
         onClick={() => handleGetRandom()}
-      ></i>
+      ></i> */}
     </div>
   );
 }
